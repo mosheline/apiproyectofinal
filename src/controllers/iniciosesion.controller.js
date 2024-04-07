d1a2fa13be829b3f597fe1587874d2b4bb796706
mosheline <moshe.felixg@gmail.com>
2024-04-07 16:50:54 -0500

    modificando login

--- src/controllers/iniciosesion.controller.js
+++ src/controllers/iniciosesion.controller.js
@@ -1,8 +1,8 @@
 import { pool } from "../db.js"
 
 export const getIniciarSesion = async (req, res) => {
-    const {usuario, contrasena} = req.body
-    const [rows] = await pool.query("CALL AutenticarUsuario(?, ?);",[usuario, contrasena])
+    const {docidentidad, contrasena} = req.body
+    const [rows] = await pool.query("CALL AutenticarUsuario(?, ?);",[docidentidad, contrasena])
     const resultado = rows[0][0];   
     resultado.rpta = (resultado.rpta === "true");
     res.json(resultado);
