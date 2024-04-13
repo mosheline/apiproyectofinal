885a151377509b29a7898a5410f27336fd5e7afa
mosheline <moshe.felixg@gmail.com>
2024-04-13 03:48:01 -0500

    modificando login idusuario string

--- src/controllers/iniciosesion.controller.js
+++ src/controllers/iniciosesion.controller.js
@@ -5,6 +5,7 @@ export const postIniciarSesion = async (req, res) => {
     const [rows] = await pool.query("CALL AutenticarUsuario(?, ?);",[docidentidad, contrasena])
     const resultado = rows[0][0];   
     resultado.rpta = (resultado.rpta === "true");
+    resultado.idusuario = resultado.idusuario.toString();
     res.json(resultado);
 
 }
